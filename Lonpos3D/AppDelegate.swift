ab4a7189f75e2e3454008ce5faa4d09abc8f2e9d
Chris Dong <cdong@central1.com>
2021-08-22 15:43:32 -0700

    Initial Commit

---
+++ Lonpos3D/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Lonpos3D
+//
+//  Created by Wei Dong on 2021-08-22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
